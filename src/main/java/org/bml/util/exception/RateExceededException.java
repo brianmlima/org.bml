c9fc00b97f42d170bf5e88938dd15ee789ba7cec
brianmlima <brianmlima@192.168.1.52>
2013-07-14 14:17:22 -0400

    Added auto licence handler plugin

--- src/main/java/org/bml/util/exception/RateExceededException.java
+++ src/main/java/org/bml/util/exception/RateExceededException.java
@@ -1,6 +1,29 @@
 
 package org.bml.util.exception;
 
+/*
+ * #%L
+ * orgbml
+ * %%
+ * Copyright (C) 2008 - 2013 Brian M. Lima
+ * %%
+ * This file is part of org.bml.
+ * 
+ * org.bml is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * org.bml is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with org.bml.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 /**
  *
  * @author brianmlima
