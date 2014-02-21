077acd5a9bcc652611e6ef1d633b9b5308297b6d
brianmlima <brianmlima@10.0.1.10>
2014-02-20 19:12:31 -0500

    Added Licenses using maven plugin

--- src/main/java/org/bml/util/elasticconsumer/zkcontrol/DataMonitor.java
+++ src/main/java/org/bml/util/elasticconsumer/zkcontrol/DataMonitor.java
@@ -1,6 +1,29 @@
 
 package org.bml.util.elasticconsumer.zkcontrol;
 
+/*
+ * #%L
+ * org.bml
+ * %%
+ * Copyright (C) 2006 - 2014 Brian M. Lima
+ * %%
+ * This file is part of ORG.BML.
+ * 
+ *     ORG.BML is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     ORG.BML is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with ORG.BML.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 import java.util.Arrays;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
--- src/main/java/org/bml/util/exception/OverloadedException.java
+++ src/main/java/org/bml/util/exception/OverloadedException.java
@@ -1,5 +1,28 @@
 package org.bml.util.exception;
 
+/*
+ * #%L
+ * org.bml
+ * %%
+ * Copyright (C) 2006 - 2014 Brian M. Lima
+ * %%
+ * This file is part of ORG.BML.
+ * 
+ *     ORG.BML is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     ORG.BML is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with ORG.BML.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
 /**Thrown when a service is overloaded. There is a distinct difference between
  * RateExceeded which is when a third party imposed limit is reached and this 
  * exception which is used to describe a condition where the service being 
