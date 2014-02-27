202bac1c86b78c3d18ef24693c28b6fc6ce64123
brianmlima <brianmlima@10.0.1.11>
2014-02-27 13:19:36 -0500

    Renamed DataContainer to RequestDataContainer and removed date range enforcing code. This code was a leftover from a DateFormat threading issue that has been solved.

---
+++ src/main/java/org/bml/util/RequestDataContainer.java
@@ -0,0 +1,63 @@
+
+package org.bml.util;
+
+/*
+ * #%L
+ * org.bml
+ * %%
+ * Copyright (C) 2006 - 2014 Brian M. Lima
+ * %%
+ * This file is part of ORG.BML.
+ * 
+ *     ORG.BML is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     ORG.BML is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with ORG.BML.  If not, see <http://www.gnu.org/licenses/>.
+ * #L%
+ */
+
+import java.util.Map;
+
+/** 
+ * <p>
+ * A container for data associated with an http request
+ * 
+ * 
+ * This class is populated with data from a single 
+ * pixel request and is passed through the Elastic Consumer for validation and 
+ * entry into a persistent data source.
+ * @author Brian M.Lima
+ */
+public class RequestDataContainer {
+    
+    private final Map<String,String> paramMap;
+    private final String rawRequest;
+
+    public RequestDataContainer(Map<String, String> paramMap, String rawRequest) {
+        this.paramMap = paramMap;
+        this.rawRequest = rawRequest;
+    }
+
+    /**
+     * @return the paramMap
+     */
+    public Map<String,String> getParamMap() {
+        return paramMap;
+    }
+
+    /**
+     * @return the rawRequest
+     */
+    public String getRawRequest() {
+        return rawRequest;
+    }
+
+}
